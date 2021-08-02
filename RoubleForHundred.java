20f661063dd3711d49ac2b8b0e4ecbcb91244ce5
Oxana Gunchenko <ksuper2007@gmail.com>
2021-08-02 12:38:52 +0300

    Added RoubleForHundred.java

---
+++ RoubleForHundred.java
@@ -0,0 +1,18 @@
+public class RoubleForHundred {
+    public static void main(String[] args) {
+        int account = 100;
+        int inputMoney = 1100;
+        int accountAdd = inputMoney + account;
+        int bonus;
+
+        if (inputMoney > 1000) {
+            bonus = (inputMoney)/100;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.printf("Account money %d\n", accountAdd);
+        System.out.printf("Bonus %d\n", bonus);
+        
+    }
+}
